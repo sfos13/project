aff9ae7778d3c6ceb5168f23225b27b16eeb4ec7
Nils <nils@rothamel.net>
2022-11-15 10:15:56 +0000

    Finished Viewer start

--- src/main/java/com/aor/DK/Viewer/Viewer.java
+++ src/main/java/com/aor/DK/Viewer/Viewer.java
@@ -1,5 +1,25 @@
-package com.aor.DK.Viewer;
+package com.aor.hero.viewer;
+
+import com.aor.hero.gui.GUI;
+
+import java.io.IOException;
 
 public abstract class Viewer<T> {
+    private final T model;
 
+    public Viewer(T model) {
+        this.model = model;
+    }
+
+    public T getModel() {
+        return model;
+    }
+
+    public void draw(GUI gui) throws IOException {
+        gui.clear();
+        drawElements(gui);
+        gui.refresh();
+    }
+
+    protected abstract void drawElements(GUI gui);
 }
