159c8009e2fcf4ff9e4001207515681847c88ac5
joaonevesf <joaodnfernandes99@gmail.com>
2022-11-15 09:48:42 +0000

    initial commit with Game class

---
+++ src/main/java/com/aor/DK/Game.java
@@ -0,0 +1,30 @@
+package com.aor.DK;
+
+import java.io.IOException;
+
+public class Game {
+    private State state;
+    private final LanternaGUI gui;
+
+    public Game() throws IOException {
+        this.gui = new LanternaGUI(20, 20);
+        this.state = new MenuState(new Menu());
+    }
+
+    public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException {
+        new Game().start();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    private void start() throws IOException {
+        while(this.state != null) {
+            long startTime = System.currentTimeMillis();
+
+            state.step(this, gui, startTime);
+        }
+        gui.close();
+    }
+}
