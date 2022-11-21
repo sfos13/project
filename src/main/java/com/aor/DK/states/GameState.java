79dd6b776ee572c145087ac4e25ab77205e84bd2
sfos <up202108675@up.pt>
2022-11-21 10:26:23 +0000

    Inicial states

---
+++ src/main/java/com/aor/DK/states/GameState.java
@@ -0,0 +1,19 @@
+package com.aor.DK.states;
+
+
+
+public class GameState extends State<Arena> {
+    public GameState(Arena arena) {
+        super(arena);
+    }
+
+    @Override
+    protected Viewer<Arena> getViewer() {
+        return new GameViewer(getModel());
+    }
+
+    @Override
+    protected Controller<Arena> getController() {
+        return new ArenaController(getModel());
+    }
+}
---
+++ src/main/java/com/aor/DK/states/State.java
@@ -0,0 +1,32 @@
+package com.aor.DK.states;
+
+import com.aor.DK.GUI.GUI;
+import com.aor.DK.Game;
+
+import java.io.IOException;
+
+public abstract class State<T> {
+    private final T model;
+    private final Controller<T> controller;
+    private final Viewer<T> viewer;
+
+    public State(T model) {
+        this.model = model;
+        this.viewer = getViewer();
+        this.controller = getController();
+    }
+
+    protected abstract Viewer<T> getViewer();
+
+    protected abstract Controller<T> getController();
+
+    public T getModel() {
+        return model;
+    }
+
+    public void step(Game game, GUI gui, long time) throws IOException {
+        GUI.ACTION action = gui.getNextAction();
+        controller.step(game, action, time);
+        viewer.draw(gui);
+    }
+}
