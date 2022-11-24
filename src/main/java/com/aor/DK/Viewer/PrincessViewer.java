45c53bdcc87e92eecc52b553106f03acfcddd6c3
Nils <nils@rothamel.net>
2022-11-24 16:31:50 +0000

    Fix Princes and DonkeyKong

---
+++ src/main/java/com/aor/DK/Viewer/PrincessViewer.java
@@ -0,0 +1,12 @@
+package com.aor.DK.Viewer;
+
+import com.aor.DK.GUI.GUI;
+import com.aor.DK.model.elements.DonkeyKong;
+import com.aor.DK.model.elements.Princess;
+
+public class PrincessViewer implements GenericViewer<Princess>{
+
+    public void draw(Princess princess, GUI gui) {
+        gui.drawPrincess(princess.getPosition());
+    }
+}
