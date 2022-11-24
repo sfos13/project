093fc7f90b561cb02324dac930045e0fc04233de
joaonevesf <joaodnfernandes99@gmail.com>
2022-11-24 22:33:39 +0000

    changed menu message position

--- src/main/java/com/aor/DK/Viewer/MenuViewer.java
+++ src/main/java/com/aor/DK/Viewer/MenuViewer.java
@@ -12,7 +12,7 @@ public class MenuViewer extends Viewer<Menu> {
     }
     @Override
     public void drawElements(GUI gui) {
-        gui.drawText(new Position(18, 10), getModel().getMessage(), "#FFFFFF");
+        gui.drawText(new Position(10, 10), getModel().getMessage(), "#FFFFFF");
 
         for (int i = 0; i < getModel().getNumberEntries(); i++)
             gui.drawText(
