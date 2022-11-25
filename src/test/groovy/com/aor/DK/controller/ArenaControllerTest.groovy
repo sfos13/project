4e1638b97c467a52e04673e2f914fb79508726a9
Nils <nils@rothamel.net>
2022-11-25 20:46:17 +0000

    Added a try for ArenaController Mock

---
+++ src/test/groovy/com/aor/DK/controller/ArenaControllerTest.groovy
@@ -0,0 +1,24 @@
+package com.aor.DK.controller
+
+import com.aor.DK.GUI.GUI
+import com.aor.DK.Game
+import com.aor.DK.controller.game.ArenaController
+import com.aor.DK.model.arena.Arena
+import com.aor.DK.states.MenuState
+import spock.lang.Specification
+
+class ArenaControllerTest extends Specification{
+/*
+    def 'testing step'(){
+        given:
+        def game = new Game()
+        def arena = new Arena(10, 10)
+        def arenaController = new ArenaController(arena)
+        def menuState = Mock(MenuState.class)
+        when:
+        arenaController.step(game, GUI.ACTION.QUIT,0)
+        then:
+        1 * game.setState(menuState);
+    }
+*/
+}
