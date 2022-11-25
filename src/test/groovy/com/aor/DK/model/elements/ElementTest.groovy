03c5cf58fdc4132c3cefd2f3ba744ed62be3bd5d
joaonevesf <joaodnfernandes99@gmail.com>
2022-11-25 14:07:49 +0000

    added more model tests

--- src/test/groovy/com/aor/DK/model/elements/ElementTest.groovy
+++ src/test/groovy/com/aor/DK/model/elements/ElementTest.groovy
@@ -21,4 +21,31 @@ class ElementTest extends Specification{
         then:
             m.getPosition() == p
     }
+
+    def 'get Mario velocity'() {
+        given:
+            def m = new Mario(2,3)
+        expect:
+            0f == m.getVy()
+    }
+
+    def 'set Mario velocity'() {
+        given:
+            def m = new Mario(2,3)
+        when:
+            m.setVy(2)
+        then:
+            2f == m.getVy()
+    }
+
+    def 'increment Mario velocity'() {
+        given:
+            def m = new Mario(2,3)
+        when:
+            m.setVy(2)
+            m.incrementVy(1)
+        then:
+            3f == m.getVy()
+    }
+
 }
