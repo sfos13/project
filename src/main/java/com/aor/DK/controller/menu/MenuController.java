dd4967e97e44a53f6842d142ada9d392c1c9f89e
joaonevesf <joaodnfernandes99@gmail.com>
2022-12-13 00:42:07 +0000

    trying to fix PNG

--- src/main/java/com/aor/DK/controller/menu/MenuController.java
+++ src/main/java/com/aor/DK/controller/menu/MenuController.java
@@ -8,7 +8,7 @@ import com.aor.DK.model.menu.Menu;
 import com.aor.DK.model.menu.RegisterScoreMenu;
 import com.aor.DK.states.LevelState;
 import com.aor.DK.states.MenuState;
-import com.aor.DK.viewer.Ranking.PlayerNameGUI;
+import com.aor.DK.viewer.ranking.PlayerNameGUI;
 
 import java.io.IOException;
 import java.util.List;
@@ -34,7 +34,7 @@ public class MenuController extends Controller<Menu> {
                             game.setState(null);
                         }
 
-                        if(getModel().isSelected_String("Start")) {
+                        if(getModel().isSelected_String("Start") || getModel().isSelected_String("Play Again")) {
                             game.setState(new LevelState(new Level(1)));
 
                         }
@@ -46,7 +46,7 @@ public class MenuController extends Controller<Menu> {
                             game.setState(new MenuState(new Menu("Start")));
                         }
 
-                        if (getModel().isSelected_String("Register score")) {
+                        if (getModel().isSelected_String("Register Score")) {
                             var model = (RegisterScoreMenu) getModel();
                             new PlayerNameGUI(model.getScore());
                         }
