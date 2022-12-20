195a2aa784c2b15dd036ffa060e4f9d990c91f2f
sfos <up202108675@up.pt>
2022-12-20 15:13:56 +0000

    "start and win viewer"

--- src/main/java/com/aor/DK/viewer/menu/WinViewer.java
+++ src/main/java/com/aor/DK/viewer/menu/WinViewer.java
@@ -1,15 +1,4 @@
 package com.aor.DK.viewer.menu;
 
-import com.aor.DK.GUI.GUI;
-import com.aor.DK.model.Position;
-import com.aor.DK.model.menu.Menu;
-import com.aor.DK.viewer.MenuViewer;
-
-public class WinViewer extends MenuViewer implements MenuGeneric {
-    public WinViewer(Menu menu) {
-        super(menu);
-    }
-
-    public void drawElements(GUI gui) {
-        gui.drawPrincess(new Position(20,20));}
+public class WinViewer {
 }
