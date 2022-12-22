570cf703d7e90eed69749eca6c141635f0e9144c
Nils <nils@rothamel.net>
2022-12-22 13:01:45 +0100

    Added Menu Controller tests

--- src/test/groovy/com/aor/DK/controller/menu/MenuControllerTest.groovy
+++ src/test/groovy/com/aor/DK/controller/menu/MenuControllerTest.groovy
@@ -26,7 +26,7 @@ class MenuControllerTest extends Specification{
         then:
         game.getState() == null
     }
- /*
+/*
     def 'Testing menu controller step GUI.ACTION.UP'(){
         given:
         def game = new Game()
@@ -39,14 +39,15 @@ class MenuControllerTest extends Specification{
 
     def 'Testing menu controller step GUI.ACTION.DOWN'(){
         given:
-        def game = new Game()
+        Game game = new Game()
         def menuController = new MenuController(new Menu("Start"))
         when:
         menuController.step(game,[GUI.ACTION.UP],100)
         then:
         game.getState().getModel().isSelectedNumber(1)
     }
-    */
+
+*/
     def 'Testing menu controller step select exit'(){
         given:
         def game = new Game()
@@ -56,5 +57,39 @@ class MenuControllerTest extends Specification{
         menuController.step(game,[GUI.ACTION.SELECT],100)
         then:
         game.getState() == null
+    }
+
+    def 'Testing menu controller step select Start'(){
+        given:
+        def game = new Game()
+        def menuController = new MenuController(new Menu("Start"))
+        when:
+        menuController.step(game,[GUI.ACTION.SELECT],100)
+        then:
+        game.getState().getModel().getLevel() == 1
+    }
+
+    def 'Testing menu controller step select Instructions'(){
+        given:
+        def game = new Game()
+        def menuController = new MenuController(new Menu("Start"))
+        when:
+        menuController.getModel().nextEntry()
+        menuController.step(game,[GUI.ACTION.SELECT],100)
+        then:
+        game.getState().getModel().getMod() == "Instructions"
+    }
+
+    def 'Testing menu controller step select Ranking'(){
+        given:
+        def game = Mock(Game.class)
+        def menuController = new MenuController(new Menu("Start"))
+        when:
+        menuController.getModel().nextEntry()
+        menuController.getModel().nextEntry()
+        menuController.getModel().nextEntry()
+        menuController.step(game,[GUI.ACTION.SELECT],100)
+        then:
+        1 * game.setState(_)
     }
 }
